12bb70edb61ba323b841af1706a61ca9e3eaadab
LocND <loc.nd247@gmail.com>
2015-12-14 17:20:19 +0700

    stripe

--- stripe.php
+++ stripe.php
@@ -1,11 +1,67 @@
-<form action="strip_action.php" method="POST">
-  <script
-    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-    data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-    data-amount="2000"
-    data-name="Dehired Package"
-    data-description="2 widgets ($20.00)"
-    data-image="payment.png"
-    data-locale="en_AU">
-  </script>
+<form action="stripe_action.php" method="POST" id="payment-form">
+    <span class="payment-errors"></span>
+    <div class="form-row">
+        <label>
+            <span>Card Number</span>
+            <input id="card-number" type="text" size="20" data-stripe="number" value="4242424242424242"/>
+        </label>
+    </div>
+    <div class="form-row">
+        <label>
+            <span>CVC</span>
+            <input id="card-cvc" type="text" size="4" data-stripe="cvc" />
+        </label>
+    </div>
+    <div class="form-row">
+        <label>
+            <span>Expiration (MM/YYYY)</span>
+            <input id="card-expiry-month" type="text" size="2" data-stripe="exp-month" />
+        </label>
+        <span> / </span>
+        <input id="card-expiry-year" type="text" size="4" data-stripe="exp-year" />
+    </div>
+    <button type="button" id="submit">Submit Payment</button>
 </form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+        $('#submit').click(function() {
+            $('.payment-errors').text('');
+            $('input').removeAttr('style');
+            var check = true;
+            if (!Stripe.card.validateCardNumber($('#card-number').val())) {
+                check = false;
+                $('#card-number').attr('style','border-color:red');
+            }
+            if (!Stripe.card.validateExpiry($('#card-expiry-month').val(), $('#card-expiry-year').val())) {
+                check = false;
+                $('#card-expiry-month').attr('style','border-color:red');
+                $('#card-expiry-year').attr('style','border-color:red');
+            }
+            if (!Stripe.card.validateCVC($('#card-cvc').val())) {
+                check = false;
+                $('#card-cvc').attr('style','border-color:red');
+            } 
+            if (check) {
+                Stripe.card.createToken({
+                    number: $('#card-number').val(),
+                    cvc: $('#card-cvc').val(),
+                    exp_month: $('#card-expiry-month').val(),
+                    exp_year: $('#card-expiry-year').val()
+                }, stripeResponseHandler);
+            }
+        });
+    });
+    function stripeResponseHandler(status, response) {
+        if (response.error) {
+            $('.payment-errors').text(response.error.message);
+        } else {
+            var token = response.id;
+            $('#payment-form').append($('<input type="hidden" name="stripeToken" />').val(token));
+            console.log(token);
+            $('#payment-form').submit();
+        }
+    }
+</script>
--- stripe_action.php
+++ stripe_action.php
@@ -1,3 +1,31 @@
-<pre>
-<?php print_r($_POST) ?>
-</pre>
+<?php
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+
+// Get the credit card details submitted by the form
+$token = $_POST['stripeToken'];
+
+// Create a Customer
+$customer = \Stripe\Customer::create(array(
+  "source" => $token,
+  "description" => "Example customer")
+);
+
+// Charge the Customer instead of the card
+\Stripe\Charge::create(array(
+  "amount" => 1000, // amount in cents, again
+  "currency" => "usd",
+  "customer" => $customer->id)
+);
+
+// YOUR CODE: Save the customer ID and other info in a database for later!
+
+// YOUR CODE: When it's time to charge the customer again, retrieve the customer ID!
+
+\Stripe\Charge::create(array(
+  "amount"   => 1500, // $15.00 this time
+  "currency" => "usd",
+  "customer" => $customerId // Previously stored, then retrieved
+  ));
