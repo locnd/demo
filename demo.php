2841b56b78f0d8ffefa3fc22842a8b16cc2218fc
LocND <loc.nd247@gmail.com>
2015-12-14 09:02:03 +0700

    commit

--- demo.php
+++
@@ -1,8 +0,0 @@
-<div>
-<script src="https://secure.ewaypayments.com/scripts/eCrypt.js"
-   class="eway-paynow-button"
-   data-publicapikey="epk-3C2C27F4-6CEB-4463-A92E-7DF1EF8904F1"
-   data-amount="1000"
-   data-currency="AUD" >
-</script>
-</div>
---
+++ stripe.php
@@ -0,0 +1,11 @@
+<form action="strip_action.php" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
+    data-amount="2000"
+    data-name="Dehired Package"
+    data-description="2 widgets ($20.00)"
+    data-image="payment.png"
+    data-locale="en_AU">
+  </script>
+</form>
